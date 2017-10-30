894e67f6878c5f0ebd9885fbf481889a9f8cf2e9
Peter Williams <pwil3058@gmail.com>
2017-10-30 11:16:47 +1000

    recollect: modify implementation to make it usable from within crate
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate fs2;
 #[macro_use]
 extern crate lazy_static;
 extern crate num;
+extern crate mut_static;
 extern crate regex;
 extern crate serde;
 #[macro_use]
@@ -57,11 +58,34 @@ pub mod recollect {
     type RDB = HashMap<String, String>;
 
     pub struct Recollections {
-        file_path: path::PathBuf
+        pub file_path: Option<path::PathBuf>
     }
 
     impl Recollections {
-        pub fn new(file_path: &path::Path) -> Recollections {
+        pub fn new(o_file_path: Option<&path::Path>) -> Recollections {
+            if let Some(ref file_path) = o_file_path {
+                if !file_path.exists() {
+                    if let Some(dir_path) = file_path.parent() {
+                        if !dir_path.exists() {
+                            fs::create_dir_all(&dir_path).unwrap_or_else(
+                                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                            );
+                        }
+                    }
+                    let file = fs::File::create(file_path).unwrap_or_else(
+                        |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                    );
+                    serde_json::to_writer(&file, &RDB::new()).unwrap_or_else(
+                        |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                    );
+                }
+                Recollections{file_path: Some(file_path.to_path_buf())}
+            } else {
+                Recollections{file_path: None}
+            }
+        }
+
+        pub fn set_data_file_path(&mut self, file_path: &path::Path) {
             if !file_path.exists() {
                 if let Some(dir_path) = file_path.parent() {
                     if !dir_path.exists() {
@@ -76,26 +100,30 @@ pub mod recollect {
                 serde_json::to_writer(&file, &RDB::new()).unwrap_or_else(
                     |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
                 );
-            }
-            Recollections{file_path: file_path.to_path_buf()}
+            };
+            self.file_path = Some(file_path.to_path_buf())
         }
 
         pub fn recall(&self, name: &str) -> Option<String> {
-            let file = fs::File::open(&self.file_path).unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            file.lock_shared().unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            let hash_map: RDB = serde_json::from_reader(&file).unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            file.unlock().unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            match hash_map.get(name) {
-                Some(ref s) => Some(s.to_string()),
-                None => None
+            if let Some(ref file_path) = self.file_path {
+                let file = fs::File::open(file_path).unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+                file.lock_shared().unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+                let hash_map: RDB = serde_json::from_reader(&file).unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+                file.unlock().unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+                match hash_map.get(name) {
+                    Some(ref s) => Some(s.to_string()),
+                    None => None
+                }
+            } else {
+                None
             }
         }
 
@@ -107,32 +135,64 @@ pub mod recollect {
         }
 
         pub fn remember(&self, name: &str, value: &str) {
-            let mut file = fs::OpenOptions::new()
-                .read(true)
-                .write(true)
-                .open(&self.file_path).unwrap_or_else(
+            if let Some(ref file_path) = self.file_path {
+                let mut file = fs::OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .open(file_path).unwrap_or_else(
+                        |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                    );
+                file.lock_exclusive().unwrap_or_else(
                     |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
                 );
-            file.lock_exclusive().unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            let mut hash_map: RDB = serde_json::from_reader(&file).unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            hash_map.insert(name.to_string(), value.to_string());
-            file.seek(io::SeekFrom::Start(0)).unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            file.set_len(0).unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            serde_json::to_writer(&file, &hash_map).unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
-            file.unlock().unwrap_or_else(
-                |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
-            );
+                let mut hash_map: RDB = serde_json::from_reader(&file).unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+                hash_map.insert(name.to_string(), value.to_string());
+                file.seek(io::SeekFrom::Start(0)).unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+                file.set_len(0).unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+                serde_json::to_writer(&file, &hash_map).unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+                file.unlock().unwrap_or_else(
+                    |err| panic!("{:?}: line {:?}: {:?}", file!(), line!(), err)
+                );
+            }
         }
+    }
+}
+
+pub mod recollections {
+    use std::path;
+    use recollect::*;
+    use mut_static::*;
+
+    //static mut RECOLLECTIONS: Recollections = Recollections{file_path: None};
+    lazy_static! {
+        static ref RECOLLECTIONS: MutStatic<Recollections> = {
+            MutStatic::from(Recollections::new(None))
+        };
+    }
+
+    pub fn init(file_path: &path::Path) {
+        RECOLLECTIONS.write().unwrap().set_data_file_path(file_path);
+    }
+
+    pub fn recall(name: &str) -> Option<String> {
+        RECOLLECTIONS.read().unwrap().recall(name)
+    }
+
+
+    pub fn recall_or_else(name: &str, default: &str) -> String {
+        RECOLLECTIONS.read().unwrap().recall_or_else(name, default)
+    }
+
+    pub fn remember(name: &str, value: &str) {
+        RECOLLECTIONS.read().unwrap().remember(name, value)
     }
 }
 
