c2a990cc6fb89fe273ac58aaec622d8f8c4e098d
Peter Williams <pwil3058@gmail.com>
2021-03-09 15:56:59 +1000

    pw_gix::menu_ng: new improved version of WrappedMenu
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- pw_gix/src/gtkx/menu_ng.rs
+++ pw_gix/src/gtkx/menu_ng.rs
@@ -1,6 +1,6 @@
 // Copyright 2018 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
 
-use std::rc::Rc;
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use gdk;
 use gtk;
@@ -204,5 +204,165 @@ impl ManagedMenuBuilder {
         mm.menu.show_all();
 
         mm
+    }
+}
+
+#[derive(PWO)]
+pub struct WrappedMenu {
+    menu: gtk::Menu,
+    items: RefCell<HashMap<String, gtk::MenuItem>>,
+}
+
+impl WrappedMenu {
+    pub fn menu(&self) -> gtk::Menu {
+        self.menu.clone()
+    }
+
+    pub fn menu_item(&self, name: &str) -> Option<gtk::MenuItem> {
+        if let Some(item) = self.items.borrow().get(name) {
+            Some(item.clone())
+        } else {
+            None
+        }
+    }
+
+    pub fn append_menu_item(&self, name: &str, item: &gtk::MenuItem) {
+        if let Some(_) = self
+            .items
+            .borrow_mut()
+            .insert(name.to_string(), item.clone())
+        {
+            panic!("Duplicate popup menu item name: {}", name);
+        };
+        self.menu.append(item);
+        self.menu.show_all();
+    }
+
+    pub fn insert_menu_item(&self, name: &str, item: &gtk::MenuItem, position: i32) {
+        if let Some(_) = self
+            .items
+            .borrow_mut()
+            .insert(name.to_string(), item.clone())
+        {
+            panic!("Duplicate popup menu item name: {}", name);
+        };
+        self.menu.insert(item, position);
+        self.menu.show_all();
+    }
+
+    pub fn prepend_menu_item(&self, name: &str, item: &gtk::MenuItem) {
+        if let Some(_) = self
+            .items
+            .borrow_mut()
+            .insert(name.to_string(), item.clone())
+        {
+            panic!("Duplicate popup menu item name: {}", name);
+        };
+        self.menu.prepend(item);
+        self.menu.show_all();
+    }
+
+    pub fn append_item(&self, name: &str, menu_item_spec: &MenuItemSpec) -> gtk::MenuItem {
+        let item = menu_item_spec.into();
+        self.append_menu_item(name, &item);
+
+        item
+    }
+
+    pub fn insert_item(
+        &self,
+        name: &str,
+        menu_item_spec: &MenuItemSpec,
+        position: i32,
+    ) -> gtk::MenuItem {
+        let item = menu_item_spec.into();
+        self.insert_menu_item(name, &item, position);
+
+        item
+    }
+
+    pub fn prepend_item(&self, name: &str, menu_item_spec: &MenuItemSpec) -> gtk::MenuItem {
+        let item = menu_item_spec.into();
+        self.prepend_menu_item(name, &item);
+
+        item
+    }
+
+    pub fn append_separator(&self) {
+        self.menu.append(&gtk::SeparatorMenuItem::new());
+        self.menu.show_all();
+    }
+
+    pub fn insert_separator(&self, position: i32) {
+        self.menu.insert(&gtk::SeparatorMenuItem::new(), position);
+        self.menu.show_all();
+    }
+
+    pub fn prepend_separator(&self) {
+        self.menu.prepend(&gtk::SeparatorMenuItem::new());
+        self.menu.show_all();
+    }
+
+    pub fn connect_item_activate<F: Fn(&gtk::MenuItem) + 'static>(&self, name: &str, f: F) {
+        if let Some(item) = self.items.borrow().get(name) {
+            item.connect_activate(f);
+        } else {
+            panic!("Unknown popup menu item name: {}", name);
+        }
+    }
+
+    pub fn set_sensitivities(&self, sensitivity: bool, names: &[&str]) {
+        for name in names.iter() {
+            if let Some(item) = self.items.borrow().get(*name) {
+                item.set_sensitive(sensitivity);
+            } else {
+                panic!("Unknown popup menu item name: {}", name);
+            }
+        }
+    }
+
+    pub fn set_visibilities(&self, visibility: bool, names: &[&str]) {
+        for name in names.iter() {
+            if let Some(item) = self.items.borrow().get(*name) {
+                item.set_visible(visibility);
+            } else {
+                panic!("Unknown popup menu item name: {}", name);
+            }
+        }
+    }
+
+    pub fn popup_at_event(&self, event: &gdk::EventButton) {
+        if self.items.borrow().len() > 0 {
+            self.menu.popup_easy(event.get_button(), event.get_time());
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct WrappedMenuBuilder {
+    items: Vec<(&'static str, MenuItemSpec)>,
+}
+
+impl WrappedMenuBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn items(&mut self, items: &[(&'static str, MenuItemSpec)]) -> &Self {
+        self.items = items.to_vec();
+        self
+    }
+
+    pub fn build(self) -> WrappedMenu {
+        let pm = WrappedMenu {
+            menu: gtk::Menu::new(),
+            items: RefCell::new(HashMap::<String, gtk::MenuItem>::new()),
+        };
+        for (name, menu_item_spec) in self.items.iter() {
+            pm.append_item(name, &menu_item_spec);
+        }
+        pm.menu.show_all();
+
+        pm
     }
 }
