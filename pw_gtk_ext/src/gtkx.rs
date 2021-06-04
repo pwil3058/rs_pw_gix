c44471cd1025d1e9d88031c9db0074a23c25f9a4
Peter Williams <pwil3058@bigpond.net.au>
2021-06-04 14:18:16 +1000

    pw_gtk_ext::list_view: ordinary list view
    Should be able to do generic version that covers both list types.
    
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

--- pw_gtk_ext/src/gtkx.rs
+++ pw_gtk_ext/src/gtkx.rs
@@ -1,17 +1,19 @@
 // Copyright 2021 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
 
-pub mod buffered_list_store;
-pub mod buffered_list_view;
-pub mod combo_box_text;
-pub mod dialog_user;
-#[macro_use]
-pub mod list_store;
-pub mod menu;
 #[macro_use]
 pub mod tree_model;
 #[macro_use]
 pub mod value;
+#[macro_use]
+pub mod list_store;
+
+pub mod buffered_list_store;
+pub mod buffered_list_view;
+pub mod combo_box_text;
 pub mod dialog;
+pub mod dialog_user;
+pub mod list_view;
 pub mod mapped;
+pub mod menu;
 pub mod notebook;
 pub mod paned;
