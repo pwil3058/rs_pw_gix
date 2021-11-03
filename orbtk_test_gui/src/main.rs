1da2864176d9586297eb28c80d64398b08603a73
Peter Williams <pwil3058@bigpond.net.au>
2021-11-03 13:46:09 +1000

    orbtk_test_gui: add items to GUI
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

--- orbtk_test_gui/src/main.rs
+++ orbtk_test_gui/src/main.rs
@@ -11,11 +11,25 @@ fn main() {
                 .title("Hello, OrbTk!")
                 .position((2000.0, 1000.0))
                 .size(200.0, 100.0)
+                .resizeable(true)
                 .child(
                     TextBlock::new()
                         .text("Hello, World")
                         .v_align("center")
                         .h_align("center")
+                        .build(ctx),
+                )
+                .child(
+                    ComboBox::new()
+                        .count(5)
+                        .items_builder(move |bc, index| {
+                            let text = format!("item{}", index);
+                            TextBlock::new().v_align("center").text(text).build(bc)
+                        })
+                        .selected_index(0)
+                        .on_changed("selected_index", |_states, entity| {
+                            println!("Entity: {:?}", entity);
+                        })
                         .build(ctx),
                 )
                 .build(ctx)
